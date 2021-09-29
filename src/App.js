18e31de45ce50eb8b55c14aa7f1863d777e625f4
Amarjeet singh <ajkarnawal0001@gmail.com>
2021-09-29 13:25:39 +0530

    info work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Info } from "./components/suggestion/Info";
 import { Routes } from "./routes/Routes";
 
 
 function App() {
   return (
     <>
+    <Info/>
       <Routes/>
     </>
   );
